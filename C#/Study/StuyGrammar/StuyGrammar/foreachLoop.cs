68fb06704d10c83890b5b312da0270ab89a82fe1
AnQuansheng <13632353886@163.com>
2018-06-12 16:35:09 +0800

    学习foreach语句

---
+++ C#/Study/StuyGrammar/StuyGrammar/foreachLoop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StuyGrammar
+{
+    class foreachLoop
+    {
+        int[] a = { 1, 3, 5, 7, 2, 4, 6, 8 };
+        void returnValue()
+        {
+            //var自适应类型
+            foreach (var k in a)
+            {
+                Console.WriteLine(k);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            foreachLoop p = new foreachLoop();
+            p.returnValue();
+            Console.ReadLine();
+        }
+    }
+}
