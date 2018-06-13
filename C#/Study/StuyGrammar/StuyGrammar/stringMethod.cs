317541f2887731760a2dd34a502edd3ed964b6c9
AnQuansheng <13632353886@163.com>
2018-06-13 16:06:07 +0800

    学习操作string类型的方法

---
+++ C#/Study/StuyGrammar/StuyGrammar/stringMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StuyGrammar
+{
+    class stringMethod
+    {
+        static void Main(string[] args)
+        {
+            string test = "that's cool~~";
+            Console.WriteLine
+                (
+                "Length:{0}-IndexOf:{1}-Insert:{2}-Remove:{3}-Replace:{4}-Substring:{5}-Contains:{6}",
+                test.Length,
+                test.IndexOf("o"),
+                test.Insert(8, "aoa"),
+                test.Remove(5),
+                test.Replace("'", " i"),
+                test.Substring(6),
+                test.Contains("s")
+                );
+            Console.ReadLine();
+        }
+    }
+}
