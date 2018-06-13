7294bdf43fa261f8dd6044f39d318590069db6bb
AnQuansheng <13632353886@163.com>
2018-06-13 16:16:19 +0800

    增加String method的注释

--- C#/Study/StuyGrammar/StuyGrammar/stringMethod.cs
+++ C#/Study/StuyGrammar/StuyGrammar/stringMethod.cs
@@ -14,12 +14,25 @@ namespace StuyGrammar
             Console.WriteLine
                 (
                 "Length:{0}-IndexOf:{1}-Insert:{2}-Remove:{3}-Replace:{4}-Substring:{5}-Contains:{6}",
+                //string.Length 返回sting长度
                 test.Length,
+
+                //string.IndexOf(value) 返回对应value所在的index值
                 test.IndexOf("o"),
+
+                //string.Insert(index,value) 在index值位置插入value
                 test.Insert(8, "aoa"),
+
+                //string.Remove(index) 移除index后的字符
                 test.Remove(5),
+
+                //string.Replace(oldValue,newValue) 将oldValue用newValue替换
                 test.Replace("'", " i"),
+
+                //string.Substing（index，Length） 移除index前的字符,并保留Length长度字符，Length缺省则index后面的全部保留
                 test.Substring(6),
+    
+                //string.Contains(value) 包含value，返回ture，否则返回false
                 test.Contains("s")
                 );
             Console.ReadLine();
