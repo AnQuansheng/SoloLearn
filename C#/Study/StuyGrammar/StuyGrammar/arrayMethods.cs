9524d52b6ca4eb036d814f1aeb827d8a7eafd92e
AnQuansheng <13632353886@163.com>
2018-06-14 12:41:37 +0800

    加深对数组声明和实例的理解

--- C#/Study/StuyGrammar/StuyGrammar/arrayMethods.cs
+++ C#/Study/StuyGrammar/StuyGrammar/arrayMethods.cs
@@ -8,7 +8,39 @@ namespace StuyGrammar
 {
     class arrayMethods
     {
-        int[,,][] multidimensionArray =
+        int[,,][] multidimensionArray = new int[2, 3, 2][];
+
+        /*
+        数组的实例的index值是从0开始算起
+        数组declare的时候，Length值不会从0开始算
+
+        int[] a = new int[2]; 数组Length是2，可以填充两个数值
+        a[3] = 5; 数组的第2个值，却是3
+
+        多维数组同理，int[2,3,2][]
+        = = = = = = = = =       
+         new int[0,0,0]|         
+         new int[0,1,0]|         
+        ——————— —      
+         new int[0,1,0]|         
+         new int[0,1,1]|         
+        ——————— —      
+         new int[0,2,0]|         
+         new int[0,2,1]|         
+        = = = = = = = = =
+         new int[1,0,0]| 
+         new int[1,1,0]| 
+        ——————— —
+         new int[1,1,0]| 
+         new int[1,1,1]| 
+        ——————— —
+         new int[1,2,0]| 
+         new int[1,2,1]|   
+        = = = = = = = = =    
+                        
+        */
+
+        /*
         {
 
             //多维数组，
@@ -17,16 +49,16 @@ namespace StuyGrammar
                 //2维
                 {
                     //3维
-                    new int[] {1,99},
-                    new int[] {1,66}
+                    new int[] {1,11},
+                    new int[] {1,12}
                 },  
                 {
-                    new int[] {2,3},
-                    new int[] {2,3}
+                    new int[] {2,22},
+                    new int[] {2,23}
                 },  
                 {
-                    new int[] {3,4},
-                    new int[] {3,4}
+                    new int[] {3,33},
+                    new int[] {3,34}
                 },
             },
 
@@ -35,26 +67,44 @@ namespace StuyGrammar
                 //2维
                 {
                     //3维
-                    new int[] {1,51},
-                    new int[] {1,52}
+                    new int[] {4,44},
+                    new int[] {4,45}
                 },  
                 {
-                    new int[] {2,61},
-                    new int[] {2,62}
+                    new int[] {5,55},
+                    new int[] {5,56}
                 },  
                 {
-                    new int[] {3,71},
-                    new int[] {3,72}
+                    new int[] {6,66},
+                    new int[] {6,67}
                 },
             }
          };
+         */
 
         //利用out返回三个值
         public void Math(out int max, out int min, out int sum)
         {
-            max = multidimensionArray[0, 0, 1].Max();
-            min = multidimensionArray[0, 0, 1].Min();
-            sum = multidimensionArray[0, 1, 1].Sum();
+            
+            multidimensionArray[0, 0, 0] = new int[] {1,12 };
+            multidimensionArray[0, 0, 1] = new int[] {1,13 };
+            multidimensionArray[0, 1, 0] = new int[] {1,13 };
+            multidimensionArray[0, 1, 1] = new int[] {1,14 };
+            multidimensionArray[0, 2, 0] = new int[] {1,15 };
+            multidimensionArray[0, 2, 1] = new int[] {1,16 };
+            multidimensionArray[1, 0, 0] = new int[] {1,17 };
+            multidimensionArray[1, 0, 1] = new int[] {1,18 };
+            multidimensionArray[1, 1, 0] = new int[] {1,19 };
+            multidimensionArray[1, 1, 1] = new int[] {1,20 };
+            multidimensionArray[1, 2, 0] = new int[] {1,21 };
+            multidimensionArray[1, 2, 1] = new int[] {1,22 };
+
+            max = multidimensionArray[0, 0, 0].Max();
+            min = multidimensionArray[0, 0, 1].Max();
+            sum = multidimensionArray[0, 1, 1].Max();
+
+            //min = multidimensionArray[0, 0, 1].Min();
+            //sum = multidimensionArray[0, 1, 1].Sum();
         }
     }
 
