c3832b78fcc11e9b57dd213d9dc434e38ec72718
AnQuansheng <13632353886@163.com>
2018-06-15 18:43:46 +0800

    operaor学习

---
+++ C#/Study/StuyGrammar/StuyGrammar/operator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StuyGrammar
+{
+    class @operator
+    {
+        public int High { get; set; }
+        public int Width { get; set; }
+        public @operator(int h, int w)
+        {
+            High = h;
+            Width = w;
+        }
+
+        public static @operator operator& (@operator boxA, @operator boxB)
+        {
+            int h = boxA.High + boxB.High;
+            int w = boxA.Width + boxB.Width;
+            @operator res = new @operator(h,w);
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            @operator A = new @operator(2, 4);
+            @operator B = new @operator(1, 3);
+            @operator C = A & B;
+            Console.WriteLine(C.High);
+            Console.WriteLine(C.Width);
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
