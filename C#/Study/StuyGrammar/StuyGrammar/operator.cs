8f28e81924297487b7edb317d5aac640b0f93e5f
AnQuansheng <13632353886@163.com>
2018-06-15 23:17:42 +0800

    构造函数加深理解

--- C#/Study/StuyGrammar/StuyGrammar/operator.cs
+++ C#/Study/StuyGrammar/StuyGrammar/operator.cs
@@ -10,11 +10,14 @@ namespace StuyGrammar
     {
         public int High { get; set; }
         public int Width { get; set; }
-        public @operator(int h, int w)
-        {
-            High = h;
-            Width = w;
-        }
+        /*
+         * 利用够着函数给类赋值
+         * public @operator(int h, int w)
+         * {
+         *     High = h;
+         *      Width = w;
+         *   
+         */
 
         //操作符重载，类似于method
         // 返回类型 operator 操作符 （参数） {语句块}
@@ -22,14 +25,25 @@ namespace StuyGrammar
         {
             int h = boxA.High + boxB.High;
             int w = boxA.Width + boxB.Width;
-            @operator res = new @operator(h,w);
+            //利用构造函数赋值
+            //@operator res = new @operator(h,w);
+            @operator res = new @operator();
+            res.High = h;
+            res.Width = w;
             return res;
         }
 
         static void Main(string[] args)
         {
-            @operator A = new @operator(2, 4);
-            @operator B = new @operator(1, 3);
+            //利用构造函数赋值
+            //@operator A = new @operator(2, 4);
+            //@operator B = new @operator(1, 3);
+            @operator A = new @operator();
+            A.High = 2;
+            A.Width = 4;
+            @operator B = new @operator();
+            B.High = 1;
+            B.Width = 3;
             @operator C = A & B;
             Console.WriteLine(C.High);
             Console.WriteLine(C.Width);
