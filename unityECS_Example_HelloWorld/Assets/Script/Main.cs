28c6ee4f8d91a979cd2920270de67c5578365cc8
13632353886 <13632353886@163.com>
2019-01-03 20:20:30 +0800

    ECS学习

---
+++ unityECS_Example_HelloWorld/Assets/Script/Main.cs
@@ -0,0 +1,24 @@
+﻿//****************************************************************************
+// Description:
+// Author: hiramtan@live.com
+//****************************************************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private RootSystems s;
+    // Use this for initialization
+    void Start()
+    {
+        s = new RootSystems(Contexts.sharedInstance);
+        s.Initialize();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        s.Execute();
+    }
+}
---
+++ unityECS_Example_HelloWorld/Assets/Script/Systems/RootSystems.cs
@@ -0,0 +1,17 @@
+﻿//****************************************************************************
+// Description:
+// Author: hiramtan@live.com
+//****************************************************************************
+
+using Entitas;
+
+public class RootSystems : Feature
+{
+    public RootSystems(Contexts contexts)
+    {
+        Add(new CreatSystem(contexts));
+        Add(new HelloWorldSystem(contexts));
+        Add(new CleanupSystem(contexts));
+        Add(new LogMouseSystem(contexts));
+    }
+}
